811267656edf5395022bd41412ea9d2983c25ee7
Ming Yang <yougmark94@gmail.com>
2014-07-04 17:00:23 +0800

    雏形

--- Step2(2)/calcx.m
+++ Step2(2)/calcx.m
@@ -1,23 +1,22 @@
 function x = calcx(A,b)
 r = 1;
 e = 0.1;
-b = b';
-x = zeros(size(b));
+x = 0.1*ones(size(b));
 while sqrt(r'*r) > e
-    r = A*x - b
-    mask = x == 0
-    mask_r = r .* mask
-    mask_r = mask_r < 0
-    mask_r = 1 - mask_r
-    r = r .* mask_r
-    return;
-    alpha = r'*r/(r'*A*r);
+    r = A*x - b;
+    mask = x == 0;
+    mask_r = r .* mask;
+    mask_r = mask_r < 0;
+    mask_r = 1 - mask_r;
+    r = r .* mask_r;
+    
+    alpha = (r'*r)/(r'*A*r);
     mask_r_2 = r < 0;
     mask_r_2 = r .* mask_r_2;
     
     tmp = -x ./ mask_r_2;
     tmp(isnan(tmp))=inf;
-    
+    tmp(tmp==-inf) = inf;
     tmp = min(tmp);
     if (tmp < alpha) 
         alpha = tmp;
